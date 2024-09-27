1c1bec412bf0e7cc96f5c4ebe34a7d4cb7de8877
Pradumn Mandal <97424547+pradumn1999@users.noreply.github.com>
2024-09-27 19:07:52 +0530

    fhgfh

--- src/pages/Auth/Register.js
+++ src/pages/Auth/Register.js
@@ -20,6 +20,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     console.log("diksha mohan");
     console.log(process.env.REACT_APP_BASE_URL);
+    console.log("weds praduman mondal");
     e.preventDefault();
     try {
       const res = await axios.post(`${process.env.REACT_APP_BASE_URL}api/v1/auth/register`, {
